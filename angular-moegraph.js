ce6af4b6ad8a2e095e710fbc4976703740081d48
Yuanhsiang Cheng <lyforever62@hotmail.com>
2013-12-02 18:21:52 +0800

    support font

--- angular-moegraph.js
+++ angular-moegraph.js
@@ -1,29 +1,33 @@
 angular.module('ngMoegraph', [])
   .directive('ngMoegraph', function () {
     return {
-      link: function (scope, element, attrs) {
+      link: function(scope, element, attrs) {
+        var moetext, moefont;
         var img = angular.element('<img>');
+        element.addClass('moegraph');
         img.attr('width', '500px');
         img.attr('height', '500px');
         element.html('');
-        
-       scope.$watch(attrs.ngMoegraph, function (thatMoetext) {
-          var w, len, padding, total;
+        moetext = attrs.ngMoegraph;
+        moefont = attrs.font;
+        scope.$watchCollection('[moetext, moefont]', function(thatMoetext) {
+          var w, len, padding, total, font;
           padding = 0;
-          if (thatMoetext) {
-            len = thatMoetext.length;
-            w = Math.ceil(len / Math.sqrt(len * 0.5));
-            total = Math.ceil(len/w)*w;
-            if(total > len && len % 4 !== 0) {
+          if (thatMoetext[0]) {
+            font = thatMoetext[1].name;
+            len = thatMoetext.length
+            w = Math.ceil(len / Math.sqrt(len * 0.5))
+            total = Math.ceil(len / w) * w
+            if (total > len && len % 4 !== 0) {
               padding = total - len;
             }
-            for(i=0; i< padding; i++) {
-              thatMoetext+=' ';
+            for (i = 0; i < padding; i++) {
+              thatMoetext += ' ';
             }
-            img.attr('src', 'https://www.moedict.tw/'+encodeURIComponent(thatMoetext)+'.png');
+            img.attr('src', 'https://www.moedict.tw/' + encodeURIComponent(thatMoetext[0])+'.png?font='+font);
             element.append(img);
           }
         })
       }
     }
-  });
+  })
